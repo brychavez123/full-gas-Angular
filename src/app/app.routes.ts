15eb913bccb6ae026b646ca21defff6e12c80a39
bryan <bryanandres9@hotmail.com>
2026-06-25 22:46:12 -0400

    Componente de Inicio Creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio').then(m => m.InicioComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio'
+  }
+];
---
+++ src/app/pages/inicio/inicio.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.scss',
+})
+export class InicioComponent {}
